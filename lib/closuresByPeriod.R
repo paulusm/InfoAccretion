0d0b35d7ad21e437794450f578b634aedb441ce7
Paul Matthews <paulus@tintin.woodclose>
2012-08-02 15:46:21 +0100

    Updated closures graph

--- lib/closuresByPeriod.R
+++ lib/closuresByPeriod.R
@@ -8,9 +8,19 @@ closuresbyperiod <- function(){
   closures.df <- dbGetQuery(con, "SELECT period, `count(*)` as closures
   FROM z_dupe_closures_by_period
   ORDER BY period")
+  
+  questions.df <- dbGetQuery(con, "SELECT period, `count(*)` as questions
+  FROM z_questions_by_period
+  ORDER BY period")
+  
+  data.df <- questions.df[3:41,]
+  data.df$closures <- closures.df$closures
 
-  p <- ggplot(closures.df, aes(period, closures))
+  p <- ggplot(data.df, aes(period, (closures/questions)*100))
   p <- p + geom_point()
+  #p <- p + geom_point(aes(period, questions/100))
   p <- p + geom_smooth(aes(group=1))
+  p <- p +  scale_y_continuous(name="Duplicate closures as a percentage of questions")
+  p <- p + opts(axis.text.x=theme_text(angle=90,hjust=1,vjust=1))
   return(p)
 }
